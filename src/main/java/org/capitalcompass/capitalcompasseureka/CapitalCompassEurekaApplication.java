946b691672bd0351e51986591b50a5331cc9e6f6
Samuel Lee <sam.leefr@gmail.com>
2023-11-07 18:47:24 +0100

    Eureka

--- src/main/java/org/capitalcompass/capitalcompasseureka/CapitalCompassEurekaApplication.java
+++ src/main/java/org/capitalcompass/capitalcompasseureka/CapitalCompassEurekaApplication.java
@@ -2,8 +2,10 @@ package org.capitalcompass.capitalcompasseureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CapitalCompassEurekaApplication {
 
     public static void main(String[] args) {
