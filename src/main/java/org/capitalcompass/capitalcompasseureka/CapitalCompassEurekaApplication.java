aa1c094dfafc649d90612c4fad76570076060adf
Samuel Lee <sam.leefr@gmail.com>
2023-11-07 17:21:45 +0100

    First commit

---
+++ src/main/java/org/capitalcompass/capitalcompasseureka/CapitalCompassEurekaApplication.java
@@ -0,0 +1,13 @@
+package org.capitalcompass.capitalcompasseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalCompassEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalCompassEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/capitalcompass/capitalcompasseureka/CapitalCompassEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capitalcompass.capitalcompasseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalCompassEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
